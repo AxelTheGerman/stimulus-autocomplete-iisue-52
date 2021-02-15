00f8a76ae07630c1d43d335b19d6ad142f74858d
Axel Gustav <dev@axelgustav.de>
2021-02-15 12:56:53 -0800

    add autocomple controller from skypack.dev

--- app/assets/javascripts/controllers/index.js
+++ app/assets/javascripts/controllers/index.js
@@ -4,6 +4,11 @@
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 
+// import { Autocomplete } from 'stimulus-autocomplete'
+import Autocomplete from 'https://cdn.skypack.dev/stimulus-autocomplete'
+
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
 application.load(definitionsFromContext(context))
+
+application.register('autocomplete', Autocomplete)
